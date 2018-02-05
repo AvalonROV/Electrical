80992d6ec3d2aaa41e960cc8018484781b514b05
Henry O'Keeffe <picofrogs@googlemail.com>
2018-02-05 20:38:41 +0000

    Changed number of bytes per packet

--- comms.c
+++ comms.c
@@ -5,7 +5,7 @@
 
 #define LOCALPORT 8000 // This is the port to listen on
 #define LOCALIP 5 //This is the last quad of the ip address of the ROV
-#define ARRYLEN 12 //This is the length of the array (send and receive) in bytes
+#define INPUTARRYLEN 15 //This is the length of the array (send and receive) in bytes
 
 EthernetUDP udp; //In all good practice this shouldn't be global, but the Arduino language means it has to be (unless we put our main loop in setup).
 
@@ -19,22 +19,22 @@ void commsSetup(void){ // Call this in setup
 
 bool comms(uint8_t* sendArry, uint8_t* receiveArry, uint8_t* tempArry){ // Returns 1 if the receive packet has changed. Should be run at least every ms.
 	bool changeR = 0, changeS = 0; // Assume no change in the received data or in the sent data
-	uint8_t oldArry[ARRYLEN];
+	uint8_t oldArry[INPUTARRYLEN];
 	int i;
 	if(millis()%10 == 0){
-		for(i = 0; i < ARRYLEN; i++)
+		for(i = 0; i < INPUTARRYLEN; i++)
 			oldArry[i] = receiveArry[i]; // Puts the current receive array into another array
 		while(!udp.parsePacket()); // Does nothing until a packet is received
-		udp.read(receiveArry, ARRYLEN); //Reads the packet into the array
-		for(i = 0; i < ARRYLEN; i++){ // This checks for a difference in both the data to be sent and the data to send
+		udp.read(receiveArry, INPUTARRYLEN); //Reads the packet into the array
+		for(i = 0; i < INPUTARRYLEN; i++){ // This checks for a difference in both the data to be sent and the data to send
 			changeR = (oldArry[i] != receiveArry[i]) | changeR; 
 			changeS = (tempArry[i] != sendArry[i]) | changeS;
 		}
 		if(changeS){
 			udp.beginPacket(udp.remoteIP(), udp.remotePort()); //Starts a packet addressed to the sender of the last packet
-			udp.write(sendArry, ARRYLEN); // Writes the array into the packet
+			udp.write(sendArry, INPUTARRYLEN); // Writes the array into the packet
 			udp.endPacket(); //Closes packet
-			for(i = 0; i < ARRYLEN; i++)
+			for(i = 0; i < INPUTARRYLEN; i++)
 				tempArry[i] = sendArry[i]; // Changes the tempArry to match the old sendArry
 		}
 	}
